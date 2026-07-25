69bbda2d6446ee5d95786839eef90f477c5aa9f8
Kilarususmitha24 <susmithakilaru24@gmail.com>
2026-07-25 12:12:56 +0530

    Add PageNotFound component for 404 handling

---
+++ Deepskilling/Module-8-Angular/HandsOn-6-Routing-Navigation/routing-app/src/app/page-not-found/page-not-found.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import {
+  RouterLink
+} from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+
+  imports: [
+    RouterLink
+  ],
+
+  templateUrl: './page-not-found.html',
+  styleUrl: './page-not-found.css'
+})
+export class PageNotFound {
+
+}
