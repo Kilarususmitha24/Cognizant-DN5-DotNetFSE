4b5c2f412f143cc992328e8c14dd23b05ba053ad
Kilarususmitha24 <susmithakilaru24@gmail.com>
2026-07-25 09:13:08 +0530

    Create app.ts

---
+++ Deepskilling/Module-8-Angular/HandsOn-1-Angular-Basics/angular-basics/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'Angular Hands-On 1';
+  studentName = 'Susmitha Kilaru';
+  course = 'Cognizant DN5.0 DotNet FSE';
+}
